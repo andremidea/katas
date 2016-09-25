98f687b12ce3810b4c0f42610b6ae026c8a42594
André Midea <midea.andre@gmail.com>
2016-09-25 20:11:45 -0300

    remove old code

--- time_to_exposure.clj
+++ time_to_exposure.clj
@@ -37,32 +37,6 @@
 
 (def output [10 10 10 8 8 7 7 6 6 4 5 5 7 7 10 10])
 
-(defn min-half [graph left right]
-  (let [lnode (-> left first first)
-        rnode (-> right last first)
-        ldist (exposure graph lnode)
-        rdist (exposure graph rnode)]
-    (cond
-      (< ldist rdist) [:left ldist]
-      (< rdist ldist) [:right rdist]
-      :else [:draw ldist])))
-
-(defn approximate-min [graph splited draw]
-  (cond
-    (> (first draw) 3) (second draw)
-    (= (count splited) 1) (exposure graph (-> splited first first))
-    :else
-    (let [split-at? (+ 2 (first draw))
-          [left right] (split-at (-> (count splited) (/ split-at?)) splited)
-          [half distance] (min-half graph left right)]
-      (println (str "left: ") left)
-      (println (str "right: ") right)
-      (println half distance)
-      (case half
-        :left (recur graph left [0 0])
-        :right (recur graph right [0 0])
-        :draw (recur graph splited [(inc (first draw)) distance])))))
-
 (defn check-closest [graph nodes]
   (let [nodes+closeness (partition 2 (interleave nodes (map #(exposure graph %) nodes)))]
     (reduce (fn [[closest new-nodes] [node distance]]
@@ -72,11 +46,11 @@
                 (= distance closest) [closest (conj new-nodes node)]))
             [Integer/MAX_VALUE []] nodes+closeness)))
 
-(defn fufu [graph nodes last-closeness]
+(defn iterate-edges [graph nodes last-closeness]
   (let [[closest new-nodes] (check-closest graph nodes)]
     (println nodes last-closeness new-nodes closest)
     (if (>= closest last-closeness) last-closeness
         (recur graph (mapcat #(get graph %) new-nodes) closest))))
 
 (defn check-min-value [graph]
-  (fufu graph [(-> graph first first)] Integer/MAX_VALUE))
+  (iterate-edges graph [(-> graph first first)] Integer/MAX_VALUE))
